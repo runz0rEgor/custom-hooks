bb4a9f8296a2075ce8425b491be4fa8cb8e9cedb
runz0rEgor <egor31340@gmail.com>
2021-03-07 15:21:21 +0300

    Add hook useHover

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Hover from './components/Hover';
+import useHover from './hooks/useHover';
 
 function App() {
+  const ref = useRef();
+  const isYellowHovering = useHover(ref);
   return (
     <div>
       <Hover />
+      <div ref={ref} style={{ width: isYellowHovering ? 400 : 200, height: 300, background: isYellowHovering ? 'yellow' : 'pink' }}></div>
     </div>
   );
 }
--- src/components/Hover.js
+++ src/components/Hover.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import useHover from '../hooks/useHover';
 
 export default function Hover() {
+  const ref = useRef();
+  const isHovering = useHover(ref);
+
+  console.log(isHovering);
   return (
-    <div style={{ width: 300, height: 300, background: 'red' }}>
+    <div ref={ref} style={{ width: isHovering ? 400 : 200, height: 300, background: isHovering ? 'red' : 'green' }}>
 
     </div>
   )
