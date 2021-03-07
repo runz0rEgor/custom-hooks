b65f46b2ea20f8a5c4709d1ed2564ec08d99e929
runz0rEgor <egor31340@gmail.com>
2021-03-07 15:00:29 +0300

    Add src/hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hover from './components/Hover';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <Hover />
     </div>
   );
 }
---
+++ src/components/Hover.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Hover() {
+  return (
+    <div style={{ width: 300, height: 300, background: 'red' }}>
+
+    </div>
+  )
+}
